0a402a9dd4b5f9cf5c78a4d5322fff792518130e
quoc95pro <quocvu@quocs-MacBook-Pro.local>
2017-05-09 20:31:59 +0700

    ok

--- resources/views/page/cart.blade.php
+++ resources/views/page/cart.blade.php
@@ -23,6 +23,7 @@
 					<tbody>
                     @php
                     $i = 0;
+					$total=0;
                     @endphp
 						@foreach($listProduct as $product)
 							<tr class="rowId{{$i}}">
@@ -53,6 +54,7 @@
 							</tr>
                             @php
                                 $i ++;
+								$total+=$product->price*$product->qty;
                             @endphp
 						@endforeach
 					</tbody>
@@ -67,67 +69,11 @@
 
 			</div>
 			<div class="row">
-				<div class="col-sm-6">
-					<div class="chose_area">
-						<ul class="user_option">
-							<li>
-								<input type="checkbox">
-								<label>Use Coupon Code</label>
-							</li>
-							<li>
-								<input type="checkbox">
-								<label>Use Gift Voucher</label>
-							</li>
-							<li>
-								<input type="checkbox">
-								<label>Estimate Shipping & Taxes</label>
-							</li>
-						</ul>
-						<ul class="user_info">
-							<li class="single_field">
-								<label>Country:</label>
-								<select>
-									<option>United States</option>
-									<option>Bangladesh</option>
-									<option>UK</option>
-									<option>India</option>
-									<option>Pakistan</option>
-									<option>Ucrane</option>
-									<option>Canada</option>
-									<option>Dubai</option>
-								</select>
-								
-							</li>
-							<li class="single_field">
-								<label>Region / State:</label>
-								<select>
-									<option>Select</option>
-									<option>Dhaka</option>
-									<option>London</option>
-									<option>Dillih</option>
-									<option>Lahore</option>
-									<option>Alaska</option>
-									<option>Canada</option>
-									<option>Dubai</option>
-								</select>
-							
-							</li>
-							<li class="single_field zip-field">
-								<label>Zip Code:</label>
-								<input type="text">
-							</li>
-						</ul>
-						<a class="btn btn-default update" href="">Get Quotes</a>
-						<a class="btn btn-default check_out" href="">Continue</a>
-					</div>
-				</div>
-				<div class="col-sm-6">
+				<div class="col-sm-9">
 					<div class="total_area">
 						<ul>
-							<li>Cart Sub Total <span>$59</span></li>
-							<li>Eco Tax <span>$2</span></li>
-							<li>Shipping Cost <span>Free</span></li>
-							<li>Total <span>$61</span></li>
+							<li>Tổng Giá : <span id="showTotal">{{number_format($total, 0, ',', '.')}}</span></li>
+							<input value="{{$total}}" id="total" style="display: none">
 						</ul>
 							<a class="btn btn-default update" href="">Update</a>
 							<a class="btn btn-default check_out" href="">Check Out</a>
